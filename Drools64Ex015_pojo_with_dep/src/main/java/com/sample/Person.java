34838d1ab53db6a432e104d4ccc56c9cf3f70049
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2017-07-20 10:39:50 +0900

    transitive dependency example

---
+++ Drools64Ex015_pojo_with_dep/src/main/java/com/sample/Person.java
@@ -0,0 +1,38 @@
+package com.sample;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Person extends StringUtils {
+
+    private String name;
+
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isNameNumeric() {
+        return isNumeric(name);
+    }
+}
