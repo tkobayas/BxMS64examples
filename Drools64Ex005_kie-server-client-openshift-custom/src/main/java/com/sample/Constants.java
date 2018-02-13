631b6d0ba92638debae031e58364499e0b00d858
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-02-13 19:19:08 +0900

    run your own rules with Decision Server on OpenShift

---
+++ Drools64Ex005_kie-server-client-openshift-custom/src/main/java/com/sample/Constants.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+
+
+
+public class Constants {
+    
+    public static final String BASE_URL = "http://kie-tk2-tkobayas-ds-project.apps.s.nekop.io/kie-server/services/rest/server";
+    
+    public static final String GROUP_ID = "org.kie.example";
+    public static final String ARTIFACT_ID = "project3";
+    public static final String VERSION = "1.0.0-SNAPSHOT";
+    
+    public static final String CONTAINER_ID = "e733f3d9ef649dbc0c798ccf0b330374";
+    
+}
